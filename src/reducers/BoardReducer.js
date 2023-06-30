5a31c9356b19df70700ab8d4cf38e0c3b5b6f22c
mandoo1229 <123732776+mandoo1229@users.noreply.github.com>
2023-06-30 16:01:13 +0900

    Fix : localstorage에 값을 전체 삭제가 되는 현상을 수정하였습니다.

--- src/reducers/BoardReducer.js
+++ src/reducers/BoardReducer.js
@@ -18,6 +18,8 @@ export const BoardReducer = (state, action) => {
         id,
       };
     }
+    //localStroage에서 'list'와 'id'라는 키로 저장된 값을 가져와서 현재 상태에 추가한다. localstorage에 값이 없는 경우에는 빈 배여로가 0을 사용한다.
+
     case ADD_ITEM: {
       const list = [...state.list, action.item];
       const id = state.id + 1;
@@ -27,6 +29,8 @@ export const BoardReducer = (state, action) => {
         id,
       };
     }
+    //액션에 전달된 'item'을 현재 상태의 'list'배열에 추가한다. 또 현재 상태의 'id'값에 1을 더한 값을 사용한다.
+
     case UPDATE_ITEM: {
       const list = state.list.map((item) => (item.id === action.item.id ? action.item : item));
       return {
@@ -34,6 +38,8 @@ export const BoardReducer = (state, action) => {
         list,
       };
     }
+    // 현재 상태의 'list'배열에서 액션에 전달된 'id'와 일치하지 않는 항목만을 남기고 필터링한다.
+
     case DELETE_ITEM: {
       const list = state.list.filter((item) => item.id !== action.id);
       return {
@@ -41,6 +47,8 @@ export const BoardReducer = (state, action) => {
         list,
       };
     }
+    //현재 상태의 'list'배열에서 액션에 전달된 'item'의 'id'와 일치하지 않는 항목만을 남기고 필터링한다.
+
     case INCREASE_VIEWS: {
       const list = state.list.map((item) => (item.id === action.id ? { ...item, views: item.views + 1 } : item));
       return {
@@ -48,12 +56,16 @@ export const BoardReducer = (state, action) => {
         list,
       };
     }
+    // 현재 상태의 'list'배열에서 액션에 전달된 'id'와 일치하는 항목을 찾아서 'views'값을 1증가 시킨다. (조회수 기능)
+
     case CHANGE_MENU: {
       return {
         ...state,
         menu: action.menu,
       };
     }
+    //현재 상태의 'menu'값을 액션에 전달된 값으로 변경한다.
+
     default:
       return { ...state };
   }
