ebacebb61478ae033a3d77ec76808efa8c106c0b
mandoo1229 <123732776+mandoo1229@users.noreply.github.com>
2023-06-30 16:02:40 +0900

    Comment : 코드 수정 및 주석을 추가하였습니다.

--- src/Page/Write.js
+++ src/Page/Write.js
@@ -11,6 +11,7 @@ function formatDateTime(date) {
   const day = String(date.getDate()).padStart(2, '0');
   return `${year}-${month}-${day} `;
 }
+//로컬시간에 맞춰서 년도, 날짜를 문자열로 변환하여 반환하는 함수이다. 글 작성시간을 표기한다.
 
 const Write = memo(({ id, dispatch }) => {
   const item = {};
@@ -18,20 +19,25 @@ const Write = memo(({ id, dispatch }) => {
   const { title, content } = state;
   const inputTitle = useRef(null);
   const inputContent = useRef(null);
+
   const navigate = useNavigate();
+  //navigate는 페이지를 이동할 때 사용함.
 
   useEffect(() => {
     dispatch({ type: CHANGE_MENU, menu: 'Write' });
     inputTitle.current.focus();
   }, [dispatch]);
+  // 컴포넌트가 랜더링 될 때와 'dispatch'가 변경될 때 실행된다.
 
   const onClickSubmit = () => {
     if (!title) {
       alert('제목을 작성해주세요');
       inputTitle.current.focus();
+      //title에 글을 작성하지 않았을 때 실행됨
     } else if (!content) {
       alert('내용을 작성해주세요');
       inputContent.current.focus();
+      // content에 값을 입력하지 않았을 때 실행됨
     } else {
       item.id = id;
       item.title = title;
@@ -43,10 +49,13 @@ const Write = memo(({ id, dispatch }) => {
       const list = localList ? JSON.parse(localList) : [];
       list.push(item);
       localStorage.setItem('list', JSON.stringify(list));
-      localStorage.setItem('id', id + 1);
+      // localStorage.setItem('id', id + 1);
       navigate(`/detail/${item.id}`);
     }
   };
+  // onClickSubmit을 클릭했을 때 실행되는 함수입니다. 글을 작성하고, 내용을 작성했을 때 else문이 실행됩니다.
+  // 새로운 글 'item'을 생성하고, 'ADD_ITEM'액션을 디스패치하여 상태를 업데이트 합니다. 입력된 값을 localstorage에 값을 저장합니다.
+  // 이후 detail페이지로 이동합니다.
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
@@ -74,8 +83,10 @@ const Write = memo(({ id, dispatch }) => {
         onChange={onChangeInput}
       />
       <div style={{ display: 'flex', justifyContent: 'flex-end', width: '80%', marginTop: '16px' }}>
-        <Button onClick={onClickSubmit}>저장</Button>
-        <Button variant="outlined" color="error" style={{ marginLeft: '8px' }}>
+        <Button variant="contained" onClick={onClickSubmit}>
+          저장
+        </Button>
+        <Button variant="contained" color="error" style={{ marginLeft: '8px' }}>
           <Link to="/">취소</Link>
         </Button>
       </div>
